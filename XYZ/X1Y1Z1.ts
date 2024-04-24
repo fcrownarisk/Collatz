24d43f23e79246033d87704f0602a6b91a2dd77e
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-04-24 03:11:35 -0700

    Add files via upload

---
+++ XYZ/X1Y1Z1.ts
@@ -0,0 +1,8 @@
+import '../operator/add'
+import '../operator/div'
+import '../operator/mod'
+export function X1Y1Z1(){
+    NaN + NaN 
+    NaN / NaN 
+    NaN % NaN
+}
---
+++ ZYX/Z2Y2X2.ts
@@ -0,0 +1,8 @@
+import '../operator/pow'
+import '../operator/minus'
+import '../operator/multiply'
+export function Z2Y2X2(){
+    NaN ^ NaN 
+    NaN - NaN 
+    NaN * NaN
+}
